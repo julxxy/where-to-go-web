1bbeee322bdaf45c09156ffe4eb00ea00a1e99b4
julian <julxxy@outlook.com>
2024-11-04 16:37:29 +0800

    feat: Update

---
+++ src/components/ShopingCart.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+interface Props {}
+
+interface State {
+  isCartOpen: boolean
+}
+
+export class ShoppingCart extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+    this.state = {
+      isCartOpen: false,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.setState({ isCartOpen: !this.state.isCartOpen })
+          }}
+        />
+      </div>
+    )
+  }
+}
